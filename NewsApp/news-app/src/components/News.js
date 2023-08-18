39f30b0b3aeb8676a764c6a355b6bb3b3182b1df
Prakash <prakashkamble591254@gmail.com>
2023-08-18 18:44:22 +0530

    first comit

--- NewsApp/news-app/src/components/News.js
+++ NewsApp/news-app/src/components/News.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import NewsUpdate from './NewsUpdate'
 
 export class News extends Component {
   render() {
     return (
       <div>
-        
+     <NewsUpdate/>
       </div>
     )
   }
--- NewsApp/news-app/src/components/NewsUpdate.js
+++ NewsApp/news-app/src/components/NewsUpdate.js
@@ -4,7 +4,7 @@ export class NewsUpdate extends Component {
   render() {
     return (
       <div>
-        
+        <h2>NewsUpdate</h2>
       </div>
     )
   }
